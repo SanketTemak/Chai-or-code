1d2cfc8a5f71ddd3a3065771ee09bc5d872be0c8
SanketTemak <138981354+SanketTemak@users.noreply.github.com>
2024-01-18 22:15:17 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,31 @@
+const Adharno = 255033633668
+let Email = "Sankettemak9858@gmail.com"
+var city = "PUNE"
+let village = "karajgaoan"
+console.table([Adharno,Email,city, village])
+/* const is usefull for constat Value that are naver chages 
+var are restricticted to use beacase it create a error in a code
+console.table usefull print the statement in table form 
+code readibility is heigh*/
+
+
+ "use strict";
+ // alert(3+3+3)// we are using node js ,not browser
+
+ /* 2. data types 
+"use strict";  trat all js code as newer version 
+*/
+
+//data Types
+
+/*Strings = "" or ''
+ Number = 123456
+ boolean = true / False 
+ null = standalone value 
+ undefind = not defind i.e not store or assign value 
+ Symbol = find the uniqeness 
+ object = */
+
+console.log(typeof null)
+console.log(typeof Email)
+console.log(typeof Adharno)
